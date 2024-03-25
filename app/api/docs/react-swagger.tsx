f7caa1fbbf74dc0503db0f31b19abdf906724bfc
FriLave <alexandre.pezat@gmail.com>
2024-03-25 15:05:55 +0100

    fix: build

--- app/api/docs/react-swagger.tsx
+++ app/api/docs/react-swagger.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import SwaggerUI from "swagger-ui-react";
+
+import dynamic from 'next/dynamic';
+
+const SwaggerUI = dynamic(() => import('swagger-ui-react'), { ssr: false });
 
 import "swagger-ui-react/swagger-ui.css";
 
